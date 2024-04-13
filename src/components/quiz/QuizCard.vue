1c117a7265c3fd6f3cec4f5989904054e8deef20
robertlek <lecarobertandrei@gmail.com>
2024-04-13 16:11:12 +0300

    Created a new component for quiz cards and used it on the homepage to display all the quizzes.

---
+++ src/components/quiz/QuizCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="quiz-card-wrapper">
+    <div class="quiz-card m-3 flex flex-column overflow-hidden min-h-full">
+      <div class="quiz-card-header h-15rem">
+        <img :src="image ?? '/src/assets/images/default-quiz-header.jpg'" alt="Header Image" class="h-15rem w-full" />
+      </div>
+      <div class="quiz-card-body flex-grow-1 p-3">
+        <h4>{{ name }}</h4>
+        <p class="text-sm mt-2">
+          {{ description }}
+        </p>
+      </div>
+      <div class="quiz-card-footer flex flex-column justify-content-center align-items-center">
+        <Button label="Preview" severity="secondary" outlined class="mt-2 w-11" />
+        <Button label="Start Quiz" class="w-11 mt-2 mb-2" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Button from 'primevue/button';
+
+defineProps({
+  name: String,
+  description: String,
+  image: String
+});
+</script>
+
+<style scoped>
+.quiz-card {
+  border: 1px solid #d3d3d3;
+  border-radius: 5px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,73 @@
 <template>
-  <main>This is the home page.</main>
+  <main>
+    <section class="flex justify-content-between mx-3">
+      <InputGroup class="w-10">
+        <InputText placeholder="Search Quiz" />
+        <Button icon="pi pi-search" class="bg-cyan-400 border-none" />
+      </InputGroup>
+
+      <Button class="bg-cyan-400 border-none">
+        <i class="pi pi-refresh mr-2"></i>
+        <span>Refresh</span>
+      </Button>
+    </section>
+
+    <section class="mx-3 mt-3">
+      <Badge v-for="quizType in quizTypeStore.getQuizTypes" :key="quizType.id" severity="secondary" class="cursor-pointer mr-2">
+        {{ quizType.name }}
+      </Badge>
+    </section>
+
+    <section class="grid grid-nogutter mt-5">
+      <QuizCard
+        v-for="quiz in quizzes"
+        :key="quiz.id"
+        :name="quiz.name"
+        :description="quiz.description"
+        :image="quiz.image"
+        class="col-12 sm:col-12 md:col-6 lg:col-4 xl:col-3 mb-5"
+      />
+    </section>
+  </main>
 </template>
+
+<script setup lang="ts">
+import { ref, computed, onMounted } from 'vue';
+import { useQuizStore } from '@/stores/QuizStore';
+import { useQuizTypeStore } from '@/stores/QuizTypeStore';
+import QuizCard from '@/components/quiz/QuizCard.vue';
+
+import Badge from 'primevue/badge';
+import Button from 'primevue/button';
+import InputGroup from 'primevue/inputgroup';
+import InputText from 'primevue/inputtext';
+
+const quizStore = useQuizStore();
+const quizTypeStore = useQuizTypeStore();
+const quizzesAreLoading = ref(true);
+
+const quizzes = computed(() => {
+  return quizStore.getQuizzes.map((quiz) => {
+    return {
+      id: quiz.id,
+      quizType: quizTypeStore.getQuizTypeName(quiz.quizTypeId),
+      name: quiz.name,
+      description: quiz.description,
+      image: quiz.imageUrl,
+      maxScore: quiz.maxScore,
+      createdDate: new Date(quiz.createdDate).toDateString()
+    };
+  });
+});
+
+onMounted(() => {
+  loadQuizzes();
+});
+
+const loadQuizzes = async () => {
+  quizzesAreLoading.value = true;
+  await quizTypeStore.fetchQuizTypes();
+  await quizStore.fetchQuizzes();
+  quizzesAreLoading.value = false;
+};
+</script>
